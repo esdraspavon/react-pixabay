482fd3c4c2179017bd6bfff013bc84bba178d74c
Esdras Pavon <pavonesdras@gmail.com>
2019-04-18 10:38:03 -0300

    read input form and set state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import Form from "./components/Form";
 
 class App extends Component {
+  state = {
+    query: ""
+  };
+
+  searchInApi = () => {
+    console.log("desde search api");
+  };
+
+  search = query => {
+    this.setState({ query }, () => this.searchInApi());
+  };
+
   render() {
     return (
       <div className="app container">
         <div className="jumbotron">
           <p className="lead text-center">Buscador de imágenes</p>
-          <Form />
+          <Form search={this.search} />
         </div>
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  queryRef = React.createRef();
+
+  getData = e => {
+    e.preventDefault();
+
+    //Leer el valor
+    const query = this.queryRef.current.value;
+
+    this.props.search(query);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.getData}>
         <div className="row">
           <div className="form-group col-md-8">
             <input
               type="text"
               className="form-control form-control-lg"
               placeholder="Busca tu imagen, ejemplo: Futbol"
+              ref={this.queryRef}
             />
           </div>
           <div className="form-group col-md-4">
